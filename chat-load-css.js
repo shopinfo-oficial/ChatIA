3005b2c4769ae66debb4cc314155708c1e9bbcda
CiroGomess <ciro.ggj@gmail.com>
2025-10-02 11:08:38 -0300

    updt: get css

--- chat-load-css.js
+++ chat-load-css.js
@@ -1,9 +1,4 @@
-(function loadChatCss() {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "https://shopinfo-oficial.github.io/ChatIA/chatIA.css";
-    document.head.appendChild(link);
-
-    console.log("🎨 CSS do chat injetado:", link.href);
-})();
+document.head.insertAdjacentHTML(
+  "beforeend",
+  '<link rel="stylesheet" href="https://shopinfo-oficial.github.io/ChatIA/chatIA.css">'
+);
